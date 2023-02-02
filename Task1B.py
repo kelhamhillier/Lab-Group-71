e91eb27b6b2e691667ffb77fc5e104be8ff96b44
kelhamhillier <kh745@cam.ac.uk>
2023-02-02 15:24:00 +0000

    Changes to Task format

--- Task1B.py
+++ Task1B.py
@@ -3,22 +3,25 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.utils import sorted_by_key
 from floodsystem.geo import stations_by_distance
 
+def run():
+    stations=build_station_list()
 
-stations=build_station_list()
-
-def closest_by_distance(stations, p, town=False):
-    if town==False:
-        sorted=sorted_by_key(stations_by_distance(stations, p), 1)
-    else:
-        sorted=sorted_by_key(stations_by_distance(stations, p, town=True), 2)
-    return sorted[:10]
+    def closest_by_distance(stations, p, town=False):
+        if town==False:
+            sorted=sorted_by_key(stations_by_distance(stations, p), 1)
+        else:
+            sorted=sorted_by_key(stations_by_distance(stations, p, town=True), 2)
+        return sorted[:10]
     
-def furthest_by_distance(stations, p, town=False):
-    if town==False:
-        sorted=sorted_by_key(stations_by_distance(stations, p), 1)
-    else:
-        sorted=sorted_by_key(stations_by_distance(stations, p, town=True), 2)
-    return sorted[-10:]
+    def furthest_by_distance(stations, p, town=False):
+        if town==False:
+            sorted=sorted_by_key(stations_by_distance(stations, p), 1)
+        else:
+            sorted=sorted_by_key(stations_by_distance(stations, p, town=True), 2)
+        return sorted[-10:]
  
-print("Closest 10 stations are:",closest_by_distance(stations, (52.2053, 0.1218), town=True))
-print("Furthest 10 stations are:",furthest_by_distance(stations, (52.2053, 0.1218), town=True))
+    print("Closest 10 stations are:",closest_by_distance(stations, (52.2053, 0.1218), town=True))
+    print("Furthest 10 stations are:",furthest_by_distance(stations, (52.2053, 0.1218), town=True))
+
+if __name__=="__main__":
+    run()
--- Task1C.py
+++ Task1C.py
@@ -1,4 +1,8 @@
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
-stations=build_station_list()
-print(sorted(stations_within_radius(stations, (52.2053, 0.1218), 10)))
+def run():
+    stations=build_station_list()
+    print(sorted(stations_within_radius(stations, (52.2053, 0.1218), 10)))
+
+if __name__=="__main__":
+    run()
