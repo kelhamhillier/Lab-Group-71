02dab34bb126890bdb57aedc59105b72deaeeeef
wh365 <115560920+wh365@users.noreply.github.com>
2023-02-06 00:33:27 +0000

    Create Task1E.py

---
+++ Task1E.py
@@ -0,0 +1,8 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
+def run():
+    stations=build_station_list()
+    return print(rivers_by_station_number(stations, 9))
+    
+if __name__=="__main__":
+    run()
