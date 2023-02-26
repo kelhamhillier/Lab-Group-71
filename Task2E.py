5ad6a4ee9f79299c082316d504a2244d6b59b4cf
kelhamhillier <kh745@cam.ac.uk>
2023-02-26 17:28:39 +0000

    Task 2E/F changes

--- Task2E.py
+++ Task2E.py
@@ -1,17 +1,23 @@
 from floodsystem.plot import plot_water_levels
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
 
 def run():
     stations=build_station_list()
+    update_water_levels(stations)
     five_s=stations_highest_rel_level(stations, 5)
     dt=10
     for i in range(5):
         next_stat=five_s[i]
-        dates, levels=fetch_measure_levels(next_stat.measure_id, dt=datetime.timedelta(days=dt))
-        plot_water_levels(next_stat, dates, levels)
+        for station in stations:
+            if station.name==next_stat[0]:
+                next_stat=station
+                print(station)
+                dates, levels=fetch_measure_levels(next_stat.measure_id, dt=datetime.timedelta(days=dt))
+                plot_water_levels(next_stat, dates, levels)
+                break
 
 if __name__=="__main__":
     run()
--- Task2F.py
+++ Task2F.py
@@ -1,17 +1,23 @@
 from floodsystem.plot import plot_water_level_with_fit
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
 
 def run():
     stations=build_station_list()
+    update_water_levels(stations)
     five_s=stations_highest_rel_level(stations, 5)
     dt=2
     for i in range(5):
         next_stat=five_s[i]
-        dates, levels=fetch_measure_levels(next_stat.measure_id, dt=datetime.timedelta(days=dt))
-        plot_water_level_with_fit(station, dates, levels, 4)
+        for station in stations:
+            if station.name==next_stat[0]:
+                next_stat=station
+                print(station)
+                dates, levels=fetch_measure_levels(next_stat.measure_id, dt=datetime.timedelta(days=dt))
+                plot_water_level_with_fit(next_stat, dates, levels, 4)
+                break
 
 if __name__=="__main__":
     run()
